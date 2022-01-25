abc2a57fb5676bf79e6231957be64b439909738a
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-01-25 10:12:51 -0600

    had error 'no git repository'

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+    id VARCHAR (10) NOT NULL,
+    artist VARCHAR (10) NOT NULL,
+    name VARCHAR (20),
+    release_date DATA (6),
+    sales DECIMAL (6, 4),
+    genre VARCHAR (8),
+    PRIMARY KEY (id)
+);
